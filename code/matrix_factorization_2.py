318e80a645b0d4aa96c2b3e1fa3d4df364c5cd1b
reverliu <39977517+reverliu@users.noreply.github.com>
2019-04-27 13:17:27 +0800

    Add files via upload

---
+++ code/matrix_factorization_2.py
@@ -0,0 +1,22 @@
+'''
+Date:20160411
+@author: zhaozhiyong
+'''
+
+from pylab import *
+from numpy import *
+
+data = []
+
+f = open("result")
+for line in f.readlines():
+    lines = line.strip()
+    data.append(lines)
+
+n = len(data)
+x = range(n)
+plot(x, data, color='r',linewidth=3)
+plt.title('Convergence curve')
+plt.xlabel('generation')
+plt.ylabel('loss')
+show()
